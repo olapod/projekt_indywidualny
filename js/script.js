ae86971dc61c0c7297699f9c04458702b70e2b1e
Ola Podsiadlik <alecs@op.pl>
2018-12-16 21:46:32 +0100

    Work 16.12.2018

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function toggleMenu(visible) {
+    document.querySelector('.mobile-menu-list').classList.toggle('show', visible)
+  }
+
+  document.querySelector('.hamburger').addEventListener('click', function(e) {
+    e.preventDefault();
+    toggleMenu();
+    console.log(toggleMenu());
+  });
